a1a40c178e67ed0838aa1846a3aff623d8ff632d
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-04 17:34:21 +1000

    update dialog box

--- client/src/Dialog/Dialog.tsx
+++ client/src/Dialog/Dialog.tsx
@@ -1,15 +1,24 @@
 import "./Dialog.css";
 
-type props = {
+type Props = {
   children: JSX.Element;
+  setDialogBox: (dialog: boolean) => void;
+  dialogBox: boolean;
+  title: string;
 };
 
-const Dialog = ({ children }: props) => {
+const Dialog = ({ children, setDialogBox, dialogBox, title }: Props) => {
+  const handleDialogClose = () => {
+    setDialogBox(false);
+  };
   return (
     <div>
-      <div className="dialog-wrapper">
-        <input type="text" />
-        {children}
+      <div
+        className={dialogBox ? "dialog-wrapper-show" : "dialog-wrapper-hide"}
+      >
+        <span>{title}</span>
+        {/* <span onClick={handleDialogClose}>cancel</span> */}
+        <div className="dialogbox-content">{children}</div>
       </div>
     </div>
   );
